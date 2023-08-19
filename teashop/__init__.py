62de8b89233a5e28003a2dcacc4cbc305b76e954
GeorgySk <skorobogatov@phystech.edu>
2023-08-19 20:37:35 +0200

    fix __init__.py

--- teashop/__init__.py
+++ teashop/__init__.py
@@ -1,5 +1,10 @@
-"""Library for retrieving information about teas available on Tea Shop website."""
+"""
+Library for retrieving information about teas available on Tea Shop
+website.
+"""
 
 __author__ = """Georgy Skorobogatov"""
 __email__ = 'skorobogatovgeorgy@gmail.com'
 __version__ = '0.1.1-alpha'
+
+from .teashop import fetch_teas
